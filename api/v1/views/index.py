8f19b2d88108f1f870f2366a183aa824d5bf560b
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-08 00:14:58 +0100

    Retrieve number of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,13 @@
 from flask import jsonify
 
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -11,3 +18,19 @@ def status():
     """ Status of the web server
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def count():
+    """ Get the number of each objects by type
+    """
+    objs_stat = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+        }
+
+    return jsonify(objs_stat)
